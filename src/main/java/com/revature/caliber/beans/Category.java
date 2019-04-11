2cb5542e412132e75103f15d95abe9a800ecb886
Javier Rodriguez <August112641@gmail.com>
2019-04-11 16:30:00 -0400

    Modified POM and Added Basic Swagger Config

--- src/main/java/com/revature/caliber/beans/Category.java
+++ src/main/java/com/revature/caliber/beans/Category.java
@@ -1,7 +1,18 @@
 package com.revature.caliber.beans;
 
-@Entitiy
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Entity
 @Table(name="CALIBER_CATEGORY")
+@Document(collection = "category")
 public class Category {
 	@Id
 	@Column(name="CATEGORY_ID")
--- src/main/java/com/revature/caliber/services/CategoryService.java
+++ src/main/java/com/revature/caliber/services/CategoryService.java
@@ -1,6 +1,8 @@
 package com.revature.caliber.services;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public interface CategoryService {
 
 }
