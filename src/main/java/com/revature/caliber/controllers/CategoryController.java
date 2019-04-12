f599467bd9fec6cd7d7443338dfdf8d9d04159a9
Kirill Zhdakh <kzhdakh@fullsail.edu>
2019-04-12 15:46:03 -0400

    Removed unused import

--- src/main/java/com/revature/caliber/controllers/CategoryController.java
+++ src/main/java/com/revature/caliber/controllers/CategoryController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.caliber.beans.Category;
