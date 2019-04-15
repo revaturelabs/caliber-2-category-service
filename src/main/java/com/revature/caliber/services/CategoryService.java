473c572ca9bb629c3140afe9ccd6085a7dc7e728
VanalstineDJ <VanalstineDJ@gmail.com>
2019-04-15 10:58:00 -0400

    CAL-155 added category owner service method

--- src/main/java/com/revature/caliber/services/CategoryService.java
+++ src/main/java/com/revature/caliber/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.revature.caliber.services;
 import java.util.List;
 
 import com.revature.caliber.beans.Category;
+import com.revature.caliber.beans.CategoryOwner;
 
 
 public interface CategoryService {
@@ -12,6 +13,8 @@ public interface CategoryService {
 	public List<Category> getAllCategories();
 	
 	public Category getCategory(int id);
+	
+	public List<Category> getCategoriesByCategoryOwner(CategoryOwner owner);
 	
 	public void updateCategory(Category c);
 	
